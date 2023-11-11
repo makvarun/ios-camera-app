f0b59253be730118fc8366764ec2ca3bfeba114f
makvarun <126195300+makvarun@users.noreply.github.com>
2023-11-10 22:22:00 -0800

    Initial Commit

---
+++ Camera/CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp.swift
+//  Camera
+//
+//  Created by Varun Makwana on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
